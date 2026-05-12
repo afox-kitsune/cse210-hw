bb554ffe3ebae04a83e894aa9bb749f1f70914eb
afox-kitsune <fox_alana@ymail.com>
2026-05-11 18:52:25 -0700

    request name exercise

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string feedback = Console.ReadLine();
+        int percent = int.Parse(feedback);
+        
+        string letter = "";
+
+        if (percent >=90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if (percent >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You have passed the course! You really made the grade!");
+        }
+        else
+        {
+            Console.WriteLine("As Gandulf says You shall not pass Shire-ly you'll do better next time!");
+        }
+
     }
+
 }
+
+
