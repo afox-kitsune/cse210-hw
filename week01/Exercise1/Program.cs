5fc66d8c3ec1fbe2d8fc66cee450a891fbc07fac
afox-kitsune <fox_alana@ymail.com>
2026-05-11 18:40:47 -0700

    add name exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {last}, {first} {last}.");
     }
 }
