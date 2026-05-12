5e7801f409044df5e69e4b3e09683bc1b5b90920
afox-kitsune <fox_alana@ymail.com>
2026-05-11 18:57:29 -0700

    numbers sort

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int enterNumber = -1;
+    
+        while (enterNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            enterNumber = int.Parse(Console.ReadLine());
+
+            if (enterNumber != 0)
+            {
+                numbers.Add(enterNumber);}
+            
+        } 
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+        }   
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+        foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine($"The maximum number is: {max}");   
     }
 }
