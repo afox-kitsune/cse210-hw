ef7d476b08cfca41828214d33d9f4fa9cfaa6eb8
afox-kitsune <fox_alana@ymail.com>
2026-05-13 18:08:24 -0700

    Week2 Learning Activty

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+
+        job1._jobTitle = "Financial Coordinator";
+        job1._company = "Wright Orthodontics";
+        job1._startYear = 2015;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Data Analyst/Project Coordinator";
+        job2._company = "Oneil";
+        job2._startYear = 2022;   
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name= "Alana Fox";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();                                          
     }
 }
